0dcee689acb0ecb1c298389e61decdd443671da4
wfd <752387064@qq.com>
2023-12-07 17:15:58 +0800

    更新

--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -17,7 +17,6 @@ for(var i = 0;i < configs.length;i++){
 //获取配置
 entryHandler.prototype.getRandomCfg = function(msg, session, next) {
   var data = config_datas[Math.floor(Math.random() * config_datas.length)]
-  console.log("getRandomCfg",data)
   next(null,{flag : true,data : data})
 }
 //登陆账号
